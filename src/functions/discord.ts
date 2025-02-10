a37e295b4c277b096ced744d2bfa5f8513de3196
Tal Ben Adon <talbenadon2@gmail.com>
2025-02-10 21:24:53 +0900

    updated discord.ts getGatewayResponse, added interface and try-catch

--- src/functions/discord.ts
+++ src/functions/discord.ts
@@ -1,7 +1,21 @@
 const discordBaseAPI = "https://discord.com/api/v10/"
 
-export async function getGateway() {
-    const response = await fetch(`${discordBaseAPI}/gateway`)
-    const data = await response.json()
-    return data
+interface GatewayResponse {
+    url: string
+}
+
+export async function getGateway(): Promise<GatewayResponse> {
+    try {
+        const response = await fetch(`${discordBaseAPI}/gateway`)
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch gateway: ${response.status} ${response.statusText}`);
+
+        }
+        const data: GatewayResponse = await response.json()
+        return data
+    } catch (error) {
+        console.error("Error fetching Discord Gateway:", error)
+        throw error
+    }
 }
