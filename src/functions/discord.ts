6d02ebb32952091688a01958c87c7904bbe9e8f0
Tal Ben Adon <talbenadon2@gmail.com>
2025-02-10 22:25:18 +0900

    updated gateway res interface

--- src/functions/discord.ts
+++ src/functions/discord.ts
@@ -1,14 +1,22 @@
 const discordBaseAPI = "https://discord.com/api/v10/"
-const token = process.env.DISCORD_TOKEN
-console.log(token)
+const discordToken = process.env.DISCORD_TOKEN
+
 interface GatewayResponse {
     url: string
+    session_start_limit: object
+    shards: number
 }
 
+// *** gets the websocket connection gateway URL *** //
 
 export async function getGateway(): Promise<GatewayResponse> {
     try {
-        const response = await fetch(`${discordBaseAPI}/gateway`)
+        const response = await fetch(`${discordBaseAPI}/gateway/bot`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bot ${discordToken}`
+            }
+        })
 
         if (!response.ok) {
             throw new Error(`Failed to fetch gateway: ${response.status} ${response.statusText}`);
